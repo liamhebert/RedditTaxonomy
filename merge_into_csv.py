8d71d618a414daa080c7cf7ddceded1360363bc2
liamhebert <liam.hebert@uwaterloo.ca>
2023-11-30 18:44:03 -0500

    fixing merge py

--- merge_into_csv.py
+++ merge_into_csv.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 def merge_into_csv(data, files, level=0):
     for file in files:
-        catagories = file.strip('.txt').split('/')
-        primary = file[0]
-        secondary = file[1] if len(file) > 1 else 'NA'
-        tertiary = file[2] if len(file) > 2 else 'NA'
-        quadternary = file[3] if len(file) > 3 else 'NA'
+        catagories = file.strip('.txt').split('\\')
+        print(file)
+        primary = catagories[1]
+        secondary = catagories[2] if len(catagories) > 2 else 'NA'
+        tertiary = catagories[3] if len(catagories) > 3 else 'NA'
+        quadternary = catagories[4] if len(catagories) > 4 else 'NA'
         with open(file, 'r') as read:
             for line in read:
                 line = line.strip('\n')
